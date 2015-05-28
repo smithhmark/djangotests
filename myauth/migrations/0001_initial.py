261f6e0993ac72802c08b3e69cfc15b7dcd3de57
Mark Smith <smith.h.mark@gmail.com>
2015-05-28 15:40:23 -0700

    creating custom user model/class

---
+++ myauth/migrations/0001_initial.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Account',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('password', models.CharField(verbose_name='password', max_length=128)),
+                ('last_login', models.DateTimeField(verbose_name='last login', blank=True, null=True)),
+                ('email', models.EmailField(max_length=254, unique=True)),
+                ('username', models.CharField(max_length=40, unique=True)),
+                ('first_name', models.CharField(blank=True, max_length=40)),
+                ('last_name', models.CharField(blank=True, max_length=40)),
+                ('tag_line', models.CharField(blank=True, max_length=140)),
+                ('is_admin', models.BooleanField(default=False)),
+                ('created_at', models.DateTimeField(auto_now_add=True)),
+                ('update_at', models.DateTimeField(auto_now=True)),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
+    ]
--- sandbox/settings.py
+++ sandbox/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myauth',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -100,3 +101,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#overwrite base user behavior to use our Account class
+AUTH_USER_MODEL = 'myauth.Account'
